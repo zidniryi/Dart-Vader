d240c509b36f0dbd0f3221b017fcca13dd71dfdd
UiUx <uiux@UiUxs-iMac.local>
2020-01-07 12:01:30 +0700

    basic function on dart

---
+++ skywalker_dart/2.basic_function.dart
@@ -0,0 +1,11 @@
+// Define the basic function
+// Call the parameter
+printNumber(num whichNumber) {
+  print('The number is $whichNumber');
+}
+
+// Calll on main  function dart
+void main() {
+  var num = 11;
+  printNumber(num);
+}
