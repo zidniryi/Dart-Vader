02afda50e6e8470f027de3b492995ee1925846d1
UiUx <uiux@UiUxs-iMac.local>
2020-01-07 13:55:03 +0700

    Simple assert

---
+++ skywalker_dart/7.assert.dart
@@ -0,0 +1,6 @@
+// Use an assert statement to disrupt normal execution if a boolean condition is false.
+// You can find examples of assert statements throughout this tour. Here are some more:
+void main() {
+  String text = 'May the force be with you!';
+  assert(text != null);
+}
