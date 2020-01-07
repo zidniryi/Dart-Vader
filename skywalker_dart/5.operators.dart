71def70d169eccd885f79dc8484b5d1ed8260894
UiUx <uiux@UiUxs-iMac.local>
2020-01-07 13:38:52 +0700

    Operators on Dart

---
+++ skywalker_dart/5.operators.dart
@@ -0,0 +1,36 @@
+// Operator on Dart
+// unary postfix and argument definition test expr++ expr-- () [] . ?identifier
+// unary prefix multiplicative
+// additive
+// shift
+// relational and type test equality
+// bitwise AND bitwise XOR bitwise OR logical AND logical OR conditional cascade assignment
+// -expr !expr ~expr ++expr --expr * / % ~/
+// + -
+// << >>
+// >= > <= == !=
+// &
+// ^
+// < as
+// is is!
+// |
+// &&
+// ||
+// expr1 ? expr2 : expr3
+// .. =*=/=~/=%=+=-=<<=>>=&=^=|=
+
+void main() {
+  var z = 2;
+  print(z + z);
+  print(z / z);
+  print(z * z);
+  print(z - z);
+  print(z = z);
+  print(z != z);
+  print(z % z);
+  print(z < z);
+  print(z > z);
+  print(z >= z);
+  print(z <= z);
+  print(z | z);
+}
