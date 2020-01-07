8902aadcc224da87d845dc58d82d11c714c95cfc
UiUx <uiux@UiUxs-iMac.local>
2020-01-07 13:25:17 +0700

    How to write variables with clean code in Dart

---
+++ skywalker_dart/3.declare_variables.dart
@@ -0,0 +1,28 @@
+//How to declare variables on Dart
+void main() {
+  int age = 20; //This is for declare number
+  String name = 'John Doe'; //This is string
+  bool isMale = true;
+  double height = 168.8;
+
+  // Final type
+  const data = 'This is final data';
+  final gender = 'Male';
+
+// Common variable
+  var country = 'Indonesia';
+
+//  List is synomym with an Array
+  var list = [1, 2, 3];
+
+//  Maps is synomym with an Object
+  var profile = {
+    // A map literal
+    // Keys       Values
+    'firstName': 'zidni',
+    'lasNam': 'ridwan nulmuarif',
+    'gender': 'male'
+  };
+// Print here for test the variables
+  print(profile);
+}
