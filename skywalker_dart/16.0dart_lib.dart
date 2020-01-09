504310f0d8fe5127bc2bbfa9944dbdfda241340e
UiUx <uiux@UiUxs-iMac.local>
2020-01-09 17:40:28 +0700

    make lib in dart

---
+++ skywalker_dart/16.0dart_lib.dart
@@ -0,0 +1,4 @@
+// We create library
+dart_lib(num1, num2) {
+  print(num1 + num2);
+}
---
+++ skywalker_dart/16.1call_lib.dart
@@ -0,0 +1,7 @@
+// We call the library
+import '16.0dart_lib.dart';
+
+void main() {
+  // call that
+  dart_lib(20, 20);
+}
